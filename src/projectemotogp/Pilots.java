4b1312874b45ffc5c79995b15f406dc1a7afd3cd
David <David@192.168.203.104>
2018-03-20 09:42:10 +0100

    El primer pas!

---
+++ src/projectemotogp/Pilots.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectemotogp;
+
+/**
+ *
+ * @author David
+ */
+public class Pilots {
+    
+    
+    private String nom;
+    private String cognoms;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognoms() {
+        return cognoms;
+    }
+
+    public void setCognoms(String cognoms) {
+        this.cognoms = cognoms;
+    }
+    
+    
+    
+    
+}
