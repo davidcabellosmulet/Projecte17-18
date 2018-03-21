0cf3286ef544952d0c75141bc4bbd4ef765465bb
David <David@192.168.203.104>
2018-03-21 10:09:56 +0100

    obejctes pilots fets

--- src/projectemotogp/Pilots.java
+++ src/projectemotogp/Pilots.java
@@ -5,15 +5,21 @@
  */
 package projectemotogp;
 
+
+
 /**
  *
  * @author David
  */
 public class Pilots {
-    
-    
-    private String nom;
-    private String cognoms;
+
+    public Categoria[] getPilot() {
+        return Pilot;
+    }
+
+    public void setPilot(Categoria[] Pilot) {
+        this.Pilot = Pilot;
+    }
 
     public String getNom() {
         return nom;
@@ -23,15 +29,73 @@ public class Pilots {
         this.nom = nom;
     }
 
-    public String getCognoms() {
-        return cognoms;
+    public String getCognom() {
+        return cognom;
     }
 
-    public void setCognoms(String cognoms) {
-        this.cognoms = cognoms;
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
     }
-    
-    
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getEquip() {
+        return equip;
+    }
+
+    public void setEquip(String equip) {
+        this.equip = equip;
+    }
+
+    public boolean isResidencia() {
+        return residencia;
+    }
+
+    public void setResidencia(boolean residencia) {
+        this.residencia = residencia;
+    }
+
+    public double getDataNaixement() {
+        return dataNaixement;
+    }
+
+    public void setDataNaixement(double dataNaixement) {
+        this.dataNaixement = dataNaixement;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(int estatura) {
+        this.estatura = estatura;
+    }
+ 
+  
+    public enum Categoria {MOTOGP, MOTO2, MOTO3,};
+    Categoria[] Pilot = Categoria.values();
+    String nom;
+    String cognom;
+    int dorsal;
+    String equip;
+    boolean residencia;
+    double dataNaixement;
+    String apodo;
+    int estatura;
     
     
 }
