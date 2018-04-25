a1a8a357c2f16fd79930fb90e6e5a0ab06b2347e
David <David@192.168.203.104>
2018-04-25 12:29:54 +0200

    Nova Actualizació

--- src/projectemotogp/ProjecteMotoGp.java
+++ src/projectemotogp/ProjecteMotoGp.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  *
  * @author David
  */
+// anem fent
 public class ProjecteMotoGp {
     //Numero de caselles maxim de l'array
     private static final int MAX_PILOTS = 10;
